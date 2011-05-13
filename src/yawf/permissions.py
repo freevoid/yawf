082994029fc2a26beed487381bf65c2c61998d1b
Nikolay Zakharov <nikolay.desh@gmail.com>
2011-05-14 02:39:34 +0400

    Initial support for complex permission checkers (with |, & and ~)

--- src/yawf/permissions.py
+++ src/yawf/permissions.py
@@ -1,31 +1,99 @@
 # -*- coding: utf-8 -*-
 
-
-def join_perms(*perm_checkers):
-    '''
-    Join all checkers in perm_checkers in single with logical AND rule.
-
-    Returns single checker function.
-    '''
-    # TODO: make JoinedChecker class that encapsulates join logic to be
-    # able to use check cache when checking for available messages
-    return lambda obj, sender: all(c(obj, sender) for c in perm_checkers)
-
-
 allow_to_all = lambda obj, sender: True
 restrict_to_all = lambda obj, sender: False
 
-class PermissionChecker(object):
+
+class BasePermissionChecker(object):
+
+    def __init__(self, *checkers):
+        self._checkers = list(checkers)
+        super(BasePermissionChecker, self).__init__()
+
+    def fill_cache(self, obj, sender):
+        cache = {}
+        for c in set(self.get_atomical_checkers()):
+            cache[c] = c(obj, sender)
+        return cache
+
+    def get_atomical_checkers(self):
+        for c in self._checkers:
+            if isinstance(c, BasePermissionChecker):
+                for child_c in c.get_atomical_checkers():
+                    yield child_c
+            else:
+                 yield c
+
+    def perform_child_checker(self, checker, obj, sender, cache):
+        if isinstance(checker, BasePermissionChecker):
+            return checker(obj, sender, cache=cache)
+        else:
+            cache_result = cache.get(checker, None)
+            return (cache_result
+                if cache_result is not None else checker(obj, sender))
+
+    def add_checker(self, checker):
+        self._checkers.append(checker)
+
+    def __and__(self, other):
+        return AndChecker(self, other)
+
+    __rand__ = __and__
+
+    def __invert__(self):
+        return NotChecker(self)
+
+    def __or__(self, other):
+        return OrChecker(self, other)
+
+    __ror__ = __or__
+
+
+class AndChecker(BasePermissionChecker):
 
     def __call__(self, obj, sender, cache=None):
         if cache is None:
-            cache = {}
+            cache = self.fill_cache(obj, sender)
+
+        return all(
+            self.perform_child_checker(c, obj, sender, cache=cache)
+            for c in self._checkers)
 
     def __and__(self, other):
-        pass
-
-    def __or__(self, other):
+        self.add_checker(other)
         return self
 
-    def __init__(self, *atomical_checkers):
-        self._atomical_checkers = atomical_checkers
+
+class OrChecker(BasePermissionChecker):
+
+    def __call__(self, obj, sender, cache=None):
+        if cache is None:
+            cache = self.fill_cache(obj, sender)
+
+        return any(
+            self.perform_child_checker(c, obj, sender, cache=cache)
+            for c in self._checkers)
+
+    def __or__(self, other):
+        self.add_checker(other)
+        return self
+
+
+class NotChecker(BasePermissionChecker):
+
+    def __init__(self, checker):
+        self._invertable_checker = checker
+        super(NotChecker, self).__init__(checker)
+
+    def __call__(self, obj, sender, cache=None):
+        if cache is None:
+            cache = self.fill_cache(obj, sender)
+
+        return not self.perform_child_checker(self._invertable_checker,
+                obj, sender, cache=cache)
+
+    def __invert__(self):
+        return self._invertable_checker
+
+
+C = AndChecker
---
+++ src/yawf/tests/permissions.py
@@ -0,0 +1,86 @@
+from django.test import TestCase
+
+from yawf.permissions import C, allow_to_all, restrict_to_all
+
+__all__ = ('PermissionsTestCase',)
+
+
+class PermissionsTestCase(TestCase):
+
+    @staticmethod
+    def call_count(func):
+        func.call_count = 0
+        def wrapper(*args):
+            func.call_count += 1
+            return func(*args)
+        wrapper.func = func
+        return wrapper
+
+    def setUp(self):
+        self.sender_is_even = self.call_count(lambda obj, sender: sender & 1 == 0)
+        self.obj_is_even = self.call_count(lambda obj, sender: obj & 1 == 0)
+
+        self.complex_checker = (
+            C(self.obj_is_even, self.sender_is_even) |
+                allow_to_all & ~C(self.sender_is_even) & allow_to_all)
+
+    def test_custom_sender(self):
+        self.assertFalse(self.sender_is_even(None, 3))
+        self.assertTrue(self.sender_is_even(None, 4))
+        self.assertFalse(self.obj_is_even(3, None))
+        self.assertTrue(self.obj_is_even(4, None))
+
+    def test_inversion(self):
+        sender_is_odd = ~C(self.sender_is_even)
+
+        self.assertTrue(sender_is_odd(None, 3))
+        self.assertFalse(sender_is_odd(None, 4))
+
+        sender_is_even = ~C(sender_is_odd)
+        self.assertFalse(sender_is_even(None, 3))
+        self.assertTrue(sender_is_even(None, 4))
+
+    def test_checker_expressions(self):
+        complex_checker = self.complex_checker
+        self.assertTrue(complex_checker(2, 2))
+        self.assertTrue(complex_checker(0, 1))
+        self.assertFalse(complex_checker(1, 2))
+
+        new_allow_to_all = allow_to_all | complex_checker
+        self.assertTrue(new_allow_to_all(2, 2))
+        self.assertTrue(new_allow_to_all(0, 1))
+        self.assertTrue(new_allow_to_all(1, 2))
+
+        new_restrict_to_all = complex_checker & restrict_to_all
+        self.assertFalse(new_restrict_to_all(2, 2))
+        self.assertFalse(new_restrict_to_all(0, 1))
+        self.assertFalse(new_restrict_to_all(1, 2))
+
+    def test_atomics(self):
+        complex_checker = self.complex_checker
+        self.assertEqual(len(set(complex_checker.get_atomical_checkers())), 3)
+
+    def test_cache(self):
+        '''
+        Call complex checker three times and check times that checker
+        functions actually been called
+        '''
+        complex_checker = self.complex_checker
+        self.assertEqual(self.sender_is_even.func.call_count, 0)
+        self.assertEqual(self.obj_is_even.func.call_count, 0)
+        complex_checker(0, 1)
+        self.assertEqual(self.sender_is_even.func.call_count, 1)
+        self.assertEqual(self.obj_is_even.func.call_count, 1)
+        complex_checker(2, 2)
+        self.assertEqual(self.sender_is_even.func.call_count, 2)
+        self.assertEqual(self.obj_is_even.func.call_count, 2)
+        complex_checker(1, 2)
+        self.assertEqual(self.sender_is_even.func.call_count, 3)
+        self.assertEqual(self.obj_is_even.func.call_count, 3)
+
+    def test_fill_cache(self):
+        cache = self.complex_checker.fill_cache(2, 2)
+        self.assertEqual(len(cache), 3)
+        self.assertTrue(self.sender_is_even in cache)
+        self.assertTrue(self.obj_is_even in cache)
+        self.assertTrue(allow_to_all in cache)
