c3e75efe75bbb0cbcbecd54266c6baef526b63d8
Nikolay Zakharov <nikolay.desh@gmail.com>
2011-05-14 02:51:04 +0400

    Proper configuration

--- src/yawf/config.py
+++ src/yawf/config.py
@@ -2,11 +2,17 @@ from django.conf import settings
 
 DEFAULT_CONFIG = {
     'WORKFLOW_TYPE_ATTR': 'workflow_type',
+    'REVISION_ATTR': 'revision',
     'INITIAL_STATE': 'init',
     'STATE_TYPE_CONSTRAINT': basestring,
     'DEFAULT_START_MESSAGE': 'start_workflow',
 }
 
-CONFIG = getattr(settings, 'YAWF_CONFIG', DEFAULT_CONFIG)
+CONFIG = DEFAULT_CONFIG.copy()
+
+custom_config = getattr(settings, 'YAWF_CONFIG', None)
+
+if custom_config:
+    CONFIG.update(custom_config)
 
 locals().update(CONFIG)
