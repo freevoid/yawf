3f1d0d6c936f95139431819d5e538873abfa697d
Nikolay Zakharov <nikolay@desh.su>
2011-08-12 19:23:09 +0400

    Perform side-effect in transaction as well

--- src/yawf/dispatch.py
+++ src/yawf/dispatch.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from yawf.config import STATE_TYPE_CONSTRAINT, REVISION_ATTR
+from yawf.config import STATE_TYPE_CONSTRAINT
 from yawf.exceptions import IllegalStateError,\
          WrongHandlerResultError, PermissionDeniedError,\
          MessageIgnored
-from yawf import get_workflow_by_instance
 from yawf.signals import message_handled
+from yawf import get_workflow_by_instance
 from yawf.messages import Message, clean_message_data
-from yawf.state_transition import perform_transition,\
-         perform_extended_transition
+from yawf.state_transition import transactional_transition
 
 logger = logging.getLogger(__name__)
 
@@ -22,8 +21,6 @@ def dispatch(obj, sender, message_id, raw_params=None):
 def dispatch_message(obj, message):
     logger.info(u"Backend got message from %s to %s: %s %s",
             message.sender, obj, message.id, message.raw_params)
-
-    object_revision = getattr(obj, REVISION_ATTR, None)
 
     # can raise WorkflowNotLoadedError
     workflow = get_workflow_by_instance(obj)
@@ -47,49 +44,25 @@ def dispatch_message(obj, message):
     # if handler returns type appropriate for state (string) - change state
     if isinstance(handler_result, STATE_TYPE_CONSTRAINT):
         if workflow.is_valid_state(handler_result):
-            old_state = obj.state
-            new_obj = perform_transition(
-                    workflow, obj.id, old_state, handler_result,
-                    old_revision=object_revision)
+            def state_transition(clarified):
+                clarified.state = handler_result
+                clarified.save()
+                return clarified
         else:
             raise IllegalStateError(handler_result)
     # if handler returns callable, perform it as single transaction
     elif callable(handler_result):
-        new_obj = perform_extended_transition(
-            workflow, obj.id, obj.state, handler_result,
-            old_revision=object_revision)
+        state_transition = handler_result
     else:
         raise WrongHandlerResultError(handler_result)
 
 
-    message_handled.send(sender=workflow.id,
+    new_obj = transactional_transition(workflow, obj, message, state_transition)
+
+    message_handled.send(
+            sender=workflow.id,
             message=message,
             instance=obj,
             new_revision=new_obj.revision)
 
-    # perform side effect action defined for transition
-    perform_side_effect(obj, new_obj,
-            message=message,
-            workflow=workflow)
     return new_obj
-
-
-def perform_side_effect(old_obj, new_obj,
-        message, workflow=None):
-
-    if workflow is None:
-        workflow = get_workflow_by_instance(new_obj.workflow_type)
-
-    action = workflow.get_action(
-            old_obj.state, new_obj.state, message.id)
-    if callable(action):
-        return action(
-            old_obj=old_obj,
-            obj=new_obj,
-            sender=message.sender,
-            params=message.params
-        )
-    else:
-        logger.warning(u"Action undefined: object id %s, state %s -> %s",
-                new_obj.id, old_obj.state, new_obj.state)
-        return None
--- src/yawf/state_transition.py
+++ src/yawf/state_transition.py
@@ -5,32 +5,21 @@ from django.db import transaction
 
 from yawf.utils import select_for_update
 from yawf.config import REVISION_ATTR
+from yawf import get_workflow_by_instance
 from yawf.exceptions import OldStateInconsistenceError,\
          ConcurrentRevisionUpdate
 
 logger = logging.getLogger(__name__)
 
 
-@transaction.commit_on_success
-def perform_transition(workflow, obj_id, old_state, new_state, old_revision):
-    obj = select_for_update(workflow.model_class.objects.filter(id=obj_id))[0]
-    new_revision = getattr(obj, REVISION_ATTR, None)
-    if old_revision is not None and new_revision != old_revision:
-        raise ConcurrentRevisionUpdate(workflow.id, obj_id, obj.state)
-    if obj.state != old_state:
-        raise OldStateInconsistenceError(obj_id, old_state, obj.state)
-
-    obj = obj.get_clarified_instance() or obj
-    obj.state = new_state
-    obj.save()
-    logger.info("Performed state transition of object %d: %s -> %s",
-            obj_id, old_state, new_state)
-    return obj
-
-
 @transaction.commit_manually
-def perform_extended_transition(workflow, obj_id, old_state, fun, old_revision):
+def transactional_transition(workflow, obj, message, state_transition):
+
     try:
+        old_revision = getattr(obj, REVISION_ATTR, None)
+        old_state = obj.state
+        obj_id = obj.id
+        old_obj = obj
         obj = select_for_update(workflow.model_class.objects.filter(id=obj_id))[0]
         new_revision = getattr(obj, REVISION_ATTR, None)
         if old_revision is not None and new_revision != old_revision:
@@ -41,12 +30,41 @@ def perform_extended_transition(workflow, obj_id, old_state, fun, old_revision):
                     old_state, obj.state)
         # all ok, perform db changes as transaction
         clarified = obj.get_clarified_instance() or obj
-        fun(clarified)
+
+        # do db changes
+        state_transition(clarified)
+
+        # perform side effect action defined for transition
+        perform_side_effect(old_obj, obj,
+                message=message,
+                workflow=workflow)
     except:
         transaction.rollback()
         raise
     else:
+
         transaction.commit()
         logger.info("Performed state transition of object %d: %s -> %s",
                 obj_id, old_state, clarified.state)
         return clarified
+
+
+def perform_side_effect(old_obj, new_obj,
+        message, workflow=None):
+
+    if workflow is None:
+        workflow = get_workflow_by_instance(new_obj.workflow_type)
+
+    action = workflow.get_action(
+            old_obj.state, new_obj.state, message.id)
+    if callable(action):
+        return action(
+            old_obj=old_obj,
+            obj=new_obj,
+            sender=message.sender,
+            params=message.params
+        )
+    else:
+        logger.warning(u"Action undefined: object id %s, state %s -> %s",
+                new_obj.id, old_obj.state, new_obj.state)
+        return None
