f00fd0fde81a340cf00030bbe2562b8c878edd41
Nikolay Zakharov <nikolay@desh.su>
2011-09-16 21:17:27 +0400

    Add new arg in message_handled signal definition

--- src/yawf/signals.py
+++ src/yawf/signals.py
@@ -1,3 +1,5 @@
 from django.dispatch import Signal
 
-message_handled = Signal(providing_args=['message', 'instance', 'new_revision'])
+message_handled = Signal(
+    providing_args=['message', 'instance', 'new_revision', 'transition_result']
+    )
