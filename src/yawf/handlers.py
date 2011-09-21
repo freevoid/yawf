64c282a539a5c1e61454583a7e591492bca63101
Nikolay Zakharov <nikolay@desh.su>
2011-09-21 13:42:09 +0400

    Add simple subclass to Handler, docstrings
    Subclass -- SimpleStateTransition. Fabric is also included.

--- src/yawf/handlers.py
+++ src/yawf/handlers.py
@@ -7,7 +7,27 @@ logger = logging.getLogger(__name__)
 
 
 class Handler(object):
+    '''
+    Basic class to express state transitions, both simple and extended.
 
+    Handler object is a callable. It gets a workflow object, message sender
+    and arbitrary keyword arguments (passed with message) and must return
+    new state id based on this information (it can return None and message
+    will be ignored).
+
+    If handler returns a callable, then this callable applies as transaction
+    on object (to change its extended state). The result of the latter
+    callable represents affected instances. If callable returns generator,
+    transition routine will coerce it to list to get those affected
+    instances.
+
+    Transitioning callable is not always must return generator or iterable.
+
+    It can return None if nothing but the workflow object is affected.
+    It can also return whatever it wants, but in this case one must avoid
+    using the builtin mechanisms for message logging or make a custom
+    signal handler for this purpose.
+    '''
     message_id = None
     message_group = None
     states_from = None
@@ -47,12 +67,26 @@ class Handler(object):
 
         super(Handler, self).__init__()
 
-    def __call__(self, obj, sender, **options):
-        return self.handle(obj, sender, **options)
+    def __call__(self, obj, sender, **kwargs):
+        return self.handle(obj, sender, **kwargs)
 
-    def handle(self, obj, sender, **options):
+    def handle(self, obj, sender, **kwargs):
         logger.warning("Message ignored (by default)")
         return None
 
     def set_handler(self, handle_func):
         self.handle = lambda self, **kwargs: handle_func(**kwargs)
+
+
+class SimpleStateTransition(Handler):
+
+    state_to = None
+
+    def handle(self, obj, sender, **kwargs):
+        return self.state_to
+
+
+def make_simple_transition(state_to):
+    class Transition(SimpleStateTransition):
+        state_to = state_to
+    return Transition
