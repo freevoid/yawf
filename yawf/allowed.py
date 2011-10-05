7d311c115178426c53908c486dacc937ce4e0107
Nikolay Zakharov <nikolay@desh.su>
2011-10-05 18:40:46 +0400

    Use state attr name from workflow in allowed.py

--- yawf/allowed.py
+++ yawf/allowed.py
@@ -5,18 +5,20 @@ from yawf import get_workflow_by_instance
 def get_allowed(sender, obj):
     workflow = get_workflow_by_instance(obj)
 
+    obj_state = getattr(obj, workflow.state_attr_name)
+
     check_result = dict(
         (c, c(obj, sender))
-        for c in workflow.get_checkers_by_state(obj.state))
+        for c in workflow.get_checkers_by_state(obj_state))
 
     messages = []
-    for checker, message in workflow.get_available_messages(obj.state):
+    for checker, message in workflow.get_available_messages(obj_state):
         if checker(obj, sender, cache=check_result):
             spec = workflow.get_message_spec(message)
             messages.append({'id': spec.id, 'title': unicode(spec), 'rank': spec.rank})
 
     resources = []
-    for resource in workflow.get_available_resources(obj.state):
+    for resource in workflow.get_available_resources(obj_state):
         if resource.permission_checker(obj, sender, cache=check_result):
             resources.append(
                 {
