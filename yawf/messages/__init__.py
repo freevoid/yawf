3013bb2a83a4fd85ef17e5b7e9f5422d2c1ab007
Nikolay Zakharov <nikolay@desh.su>
2011-10-21 03:21:05 +0400

    Add unique_id property to Message (using uuid1)

--- yawf/messages/__init__.py
+++ yawf/messages/__init__.py
@@ -1,10 +1,29 @@
+import uuid
+
 from yawf.messages.cleaning import clean_message_data
+
 
 class Message(object):
 
-    def __init__(self, sender, message_id, raw_params=None):
+    def __init__(self, sender, message_id,
+            raw_params=None, message_group=None, parent_message_id=None):
         self.sender = sender
         self.id = message_id
+        self._unique_id = None
         self.raw_params = raw_params if raw_params is not None else {}
         self.params = None
+        self.parent_message_id = parent_message_id
+        self.message_group = message_group\
+            if message_group is not None else self.unique_id
         super(Message, self).__init__()
+
+    @property
+    def unique_id(self):
+        '''
+        Property that returns the unique identifier for message.
+
+        Internally it uses UUID.
+        '''
+        if not self._unique_id:
+            self._unique_id = uuid.uuid1()
+        return self._unique_id
